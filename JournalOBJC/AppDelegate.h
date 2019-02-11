a36dc1aa928715cc19813c24a55055f23206b56f
Ryan Greenburg <ryangreenburg@mac.com>
2019-02-11 13:08:48 -0700

    Initial Commit

---
+++ JournalOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalOBJC
+//
+//  Created by RYAN GREENBURG on 2/11/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
